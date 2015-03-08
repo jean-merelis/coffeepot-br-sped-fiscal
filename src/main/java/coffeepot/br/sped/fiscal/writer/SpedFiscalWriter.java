0a14917d74f082a2b472a5720e5956251c646dc1
Jean Merelis <jean.merelis@gmail.com>
2015-03-08 19:29:29 -0300

    correcao para compatibilidade com o coffeepot-bean-wr-1.1.0-RC2

--- src/main/java/coffeepot/br/sped/fiscal/writer/SpedFiscalWriter.java
+++ src/main/java/coffeepot/br/sped/fiscal/writer/SpedFiscalWriter.java
@@ -12,9 +12,9 @@ package coffeepot.br.sped.fiscal.writer;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -114,6 +114,6 @@ public class SpedFiscalWriter {
      * utilizados.
      */
     public void cleanParsers() {
-        beanWriter.getObjectParserFactory().getParsers().clear();
+        beanWriter.getObjectParserFactory().getMappers().clear();
     }
 }

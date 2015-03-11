315dd8c865c90b2e2267f3cd2b70400edd327a2a
Jean Merelis <jean.merelis@gmail.com>
2015-03-10 23:08:49 -0300

    ...

--- src/main/java/coffeepot/br/sped/fiscal/reader/ReaderFactory.java
+++ src/main/java/coffeepot/br/sped/fiscal/reader/ReaderFactory.java
@@ -12,9 +12,9 @@ package coffeepot.br.sped.fiscal.reader;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,8 +39,7 @@ public class ReaderFactory {
         DelimitedReader reader = new DelimitedReader(r);
         reader.setDelimiter('|');
         reader.setRecordInitializator("|");
-        reader.setRemoveRecordInitializator(true);
-        reader.setCharsetName("ISO-8859-1");
+        reader.setRemoveRecordInitializator(true);        
         reader.setIgnoreUnknownRecords(false);
 
         reader.getObjectMapperFactory().getHandlerFactory().registerTypeHandlerClassFor(Enum.class, CustomEnumHandler.class);

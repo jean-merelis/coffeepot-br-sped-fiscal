687d0ebe58d9bb4c3f3fc2e9de8eeeef92f5e97d
Jeandeson O. Merelis <jean.merelis@gmail.com>
2017-03-17 12:27:42 -0300

    wraps ParseException

--- src/main/java/coffeepot/br/sped/fiscal/typeHandler/CustomEnumHandler.java
+++ src/main/java/coffeepot/br/sped/fiscal/typeHandler/CustomEnumHandler.java
@@ -12,9 +12,9 @@ package coffeepot.br.sped.fiscal.typeHandler;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -45,7 +45,7 @@ public class CustomEnumHandler extends DefaultEnumHandler {
             try{
                return EnumParser.parseEnumCodificado( type, text );
             }catch(Exception ex){
-               throw new HandlerParseException( ex.getMessage() );
+               throw new HandlerParseException( ex );
             }
         }
 

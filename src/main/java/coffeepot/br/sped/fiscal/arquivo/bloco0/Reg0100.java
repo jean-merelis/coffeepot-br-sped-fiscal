41edba491a0c52f73d50c6794e2349fcb8fe68b6
Jeandeson O. Merelis <jean.merelis@gmail.com>
2017-03-18 15:58:36 -0300

    changed property length to maxLength from Reg0100's fone and fax mapping

--- src/main/java/coffeepot/br/sped/fiscal/arquivo/bloco0/Reg0100.java
+++ src/main/java/coffeepot/br/sped/fiscal/arquivo/bloco0/Reg0100.java
@@ -12,9 +12,9 @@ package coffeepot.br.sped.fiscal.arquivo.bloco0;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,8 +47,8 @@ import lombok.Setter;
     @Field(name = "num", maxLength = 10),
     @Field(name = "compl", maxLength = 60),
     @Field(name = "bairro", maxLength = 60),
-    @Field(name = "fone", length = 11, params = {DefaultStringHandler.PARAM_FILTER_NUMBER_ONLY}),
-    @Field(name = "fax", length = 11, params = {DefaultStringHandler.PARAM_FILTER_NUMBER_ONLY}),
+    @Field(name = "fone", maxLength = 11, params = {DefaultStringHandler.PARAM_FILTER_NUMBER_ONLY}),
+    @Field(name = "fax", maxLength = 11, params = {DefaultStringHandler.PARAM_FILTER_NUMBER_ONLY}),
     @Field(name = "email"),
     @Field(name = "codMun", length = 7, align = Align.RIGHT, padding = '0', paddingIfNullOrEmpty = true)
 })
